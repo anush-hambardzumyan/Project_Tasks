d35832c37a120ea368f835d8e7613087fc9d8885
anush-hambardzumyan <foreveranham@gmail.com>
2024-02-24 17:03:53 +0400

    commit

--- tasks/level1/15.cpp
+++ tasks/level1/15.cpp
@@ -1,109 +1,138 @@
-/*Refactor the sorting algorithm selector to leverage the power of C++ classes, 
-encapsulating the sorting algorithms, file operations, and user interactions within a class structure.*/
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <sstream>
 
-class SortingStrategy
+class SortingStrategy 
 {
-    public:
-    virtual void sorting(std::vector<int>& vec) = 0;
+public:
+    virtual void sort(std::vector<int>& vec) = 0;
+    virtual ~SortingStrategy() = default;
 };
 
-class bub_sort : public SortingStrategy
+class BubbleSort : public SortingStrategy 
 {
-    public:
-    void sorting(std::vector<int>& arr)
+public:
+    void sort(std::vector<int>& arr) override 
     {
         std::cout << "Sorted by Bubble Sort: " << std::endl; 
-        for (int i = 0; i < arr.size() - 1; ++i) 
-        {
-            for (int j = 0; j < arr.size() - i - 1; ++j) 
-            {
-                if (arr[j] > arr[j + 1]) 
-                {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
     }
 };
 
-class sel_sort : public SortingStrategy
+class SelectionSort : public SortingStrategy 
 {
-    public:
-    void sort(std::vector<int>& arr) 
+public:
+    void sort(std::vector<int>& arr) override 
     {
         std::cout << "Sorted by Selection Sort: " << std::endl;
-        for (int i = 0; i < arr.size() - 1; ++i) 
-        {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.size(); ++j) 
-            {
-                if (arr[j] < arr[minIndex]) 
-                {
-                    minIndex = j;
-                }
-            }
-            if (minIndex != i) 
-            {
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
-            }
-        }    
     }
 };
 
 class FileHandler 
 {
-    public: 
-    std::vector<int> splitString(const std::string& input, char delimiter) 
+public: 
+    std::vector<int> readArrayFromFile(const std::string& filename) 
     {
-        std::vector<int> parts;
-        std::istringstream stream(input);
-        std::string part;
-        while (std::getline(stream, part, delimiter)) 
-        {
-            if (!part.empty()) 
-            {
-                parts.push_back(std::stoi(part));
-            }
-        }
-        return parts;
-    }
-
-    std::vector<int> readArrayFromFile(std::ifstream& file)
-    {
-        file.open("read.txt");
-        std::string str;
+        std::ifstream file(filename);
         std::vector<int> vec;
-        while(!file.eof())
+        if (file.is_open()) 
         {
-            std::getline(file,str);
-            vec = splitString(str,' ');
-        }
+            std::string str;
+            while (std::getline(file, str)) 
+            {
+                std::istringstream stream(str);
+                int num;
+                while (stream >> num) 
+                {
+                    vec.push_back(num);
+                }
+            }
+            file.close();
+        } 
         return vec;
     }
 
-    void saveArrayToFile(std::ifstream& file1,std::ofstream& file2)
+    void saveArrayToFile(const std::string& filename, const std::vector<int>& vec) 
     {
-        file2.open("write.txt");
-        std::vector<int> vec = readArrayFromFile(file1);
-        for(int i = 0; i < vec.size(); ++i)
+        std::ofstream file(filename);
+        if (file.is_open()) 
         {
-            file2 << vec[i] << " ";
-        }
-        file2 << '/n';
+            for (int num : vec) 
+            {
+                file << num << " ";
+            }
+            file << std::endl;
+            file.close();
+        } 
     }
 };
 
-class SortingApplication
+class SortingApplication 
 {
-    private:
-        SortingStrategy* ptr;
-        FileHandler obj;
+private:
+    SortingStrategy* sortingStrategy;
+    FileHandler fileHandler;
+
+public:
+    SortingApplication() : sortingStrategy(nullptr) {}
+
+    virtual ~SortingApplication() 
+    {
+        delete sortingStrategy;
+    }
+
+    void selectSortingStrategy() 
+    {
+        int choice;
+        std::cout << "Select sorting algorithm:" << std::endl;
+        std::cout << "1. Bubble Sort" << std::endl;
+        std::cout << "2. Selection Sort" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        delete sortingStrategy;
+        
+        switch (choice) 
+        {
+            case 1:
+                sortingStrategy = new BubbleSort();
+                break;
+            case 2:
+                sortingStrategy = new SelectionSort();
+                break;
+            default:
+                std::cout << "Invalid choice." << std::endl;
+                break;
+        }
+    }
+
+    void executeSorting(const std::string& inputFile, const std::string& outputFile) 
+    {
+        std::vector<int> data = fileHandler.readArrayFromFile(inputFile);
+        if (sortingStrategy) 
+        {
+            sortingStrategy->sort(data);
+            fileHandler.saveArrayToFile(outputFile, data);
+        } 
+
+        else 
+        {
+            std::cerr << "Sorting strategy not selected." << std::endl;
+        }
+    }
 };
+
+int main() 
+{
+    SortingApplication app;
+    app.selectSortingStrategy();
+
+    std::string inputFile; 
+    std::string outputFile;
+    std::cout << "Enter input file name: ";
+    std::cin >> inputFile;
+    std::cout << "Enter output file name: ";
+    std::cin >> outputFile;
+
+    app.executeSorting(inputFile, outputFile);
+}
