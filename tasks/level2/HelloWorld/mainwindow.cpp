39a93bbc71ee4739d8fcdbf555b3560062b5a7f6
anush-hambardzumyan <foreveranham@gmail.com>
2024-02-25 21:35:39 +0400

    commit

--- tasks/level2/HelloWorld/mainwindow.cpp
+++ tasks/level2/HelloWorld/mainwindow.cpp
@@ -7,9 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Create a QLabel and set its text
     helloLabel = new QLabel("Hello, World!", this);
-    // Set geometry for the label (position and size)
+
     helloLabel->setGeometry(100, 100, 200, 50);
 }
 
