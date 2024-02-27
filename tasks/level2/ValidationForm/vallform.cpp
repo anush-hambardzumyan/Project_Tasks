e53e0de46c5a69d9973b3b90271833f8c716eecb
anush-hambardzumyan <foreveranham@gmail.com>
2024-02-27 22:36:56 +0400

    unused files deletion

--- tasks/level2/ValidationForm/vallform.cpp
+++ tasks/level2/ValidationForm/vallform.cpp
@@ -3,34 +3,30 @@
 
 ValForm::ValForm(QWidget *parent) : QWidget(parent)
 {
-
         lineEdit1 = new QLineEdit(this);
         lineEdit2 = new QLineEdit(this);
         lineEdit3 = new QLineEdit(this);
 
-        // Create QPushButton for submission
         QPushButton *submitButton = new QPushButton("Submit", this);
 
-        // Create form layout
         QFormLayout *formLayout = new QFormLayout(this);
         formLayout->addRow("Input 1:", lineEdit1);
         formLayout->addRow("Input 2:", lineEdit2);
         formLayout->addRow("Input 3:", lineEdit3);
         formLayout->addWidget(submitButton);
 
-        // Connect the button's clicked signal to a slot for validation
         connect(submitButton, &QPushButton::clicked, this, &ValForm::validation);
     }
 
 void ValForm::validation()
 {
-        // Check if any QLineEdit widget is empty
         if (lineEdit1->text().isEmpty() || lineEdit2->text().isEmpty() || lineEdit3->text().isEmpty()) {
-            // Show error message
+
             QMessageBox::critical(this, "Error", "All fields must be filled!");
-        } else {
-            // Show success message
-            QMessageBox::warning(this, "Success", "Form submitted successfully!");
+        }
+        else
+        {
+            QMessageBox::information(this, "Success", "Form submitted successfully!");
         }
 }
 
